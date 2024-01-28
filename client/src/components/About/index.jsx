7090beb868e6400cb520b5631ae78bbf55ccf03d
Maziar Pedrami <awildroot@gmail.com>
2024-01-29 00:03:39 +0100

    navbar focus styling

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -13,19 +13,19 @@ export default function About() {
       >
         About <span className="text-deep-orange-700">SDEN</span>
       </div>
-      <div className="foundation flex p-16 gap-10">
-        <div className="text w-1/2 flex flex-col gap-4">
-          <h1 className="text-3xl font-bold text-black">
+      <div className="foundation flex p-32 gap-10">
+        <div className="text w-1/2 flex flex-col gap-4 ">
+          <h1 className="text-4xl font-bold text-black">
             Our Foundation's <span className="text-deep-orange-700">Journey</span>
           </h1>
-          <p className=" text-gray-600 mb-5 text-justify">
+          <p className=" text-gray-600 mb-5 w-5/6 text-xl ">
             The inception of SDIE Foundation was deeply influenced by the global
             commitment to the 17 Sustainable Development Goals (SDGs) set by the
             United Nations. As the world rallied to address pressing global
             challenges, we recognized the need for a focused effort to achieve
             these ambitious goals.
           </p>
-          <p className="text-gray-600 text-justify">
+          <p className="text-gray-600 w-5/6 text-xl ">
             Additionally, the political changes in Afghanistan, particularly the
             exclusion of girls from pursuing education and general existence of
             limitation to accessing education, served as a catalyst for our
@@ -33,8 +33,8 @@ export default function About() {
             urgency of our mission.
           </p>
         </div>
-        <div className="pic w-1/2">
-          <img src={Foundation} className=" w-fit"/>
+        <div className="pic w-1/2 ">
+          <img src={Foundation} className=" h-full"/>
         </div>
       </div>
     </section>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,13 +7,15 @@ import { CiSearch } from "react-icons/ci";
 function Header() {
   return (
     <>
-      <header className="flex px-2 pt-2 items-center ">
+      <header className="flex px-2 items-center space-x-96">
         <div className="brand w-1/6">
           <img src={logo} alt="NSDE Logo" />
         </div>
-        <nav className="flex pl-32 space-x-3 w-3/4">
+        <div className="flex space-x-20 w-5/6 pt-2">
+        <nav className="flex space-x-3 font-bold ">
           <Link to="/" className="w-34 py-2">Home</Link>
-          <Link to="/about" className="w-34 focus:font-bold p-2 focus:bg-blue-800 focus:text-white">About</Link>
+          <Link to="/about" className="w-34 h-20 focus:font-bold p-2 focus:bg-blue-800 focus:text-white transition-all duration-300">About</Link>
+          <Link to="/about" className="w-34 focus:font-bold p-2 focus:bg-blue-800 focus:text-white transition-all duration-300">Vision</Link>
           <Link to="/about" className="w-34 py-2">Projects</Link>
           <Link to="/about" className="w-34 py-2">Get Involved</Link>
           <Link to="/about" className="w-34 py-2">News & Blog</Link>
@@ -21,10 +23,11 @@ function Header() {
           <Link to="/about" className="w-34 py-2">Contact</Link>
           <Link to="/about" className="w-34 py-2">Events</Link>
         </nav>
-        <div className="lang and search flex items-center space-x-1  ">
+        <div className="lang and search flex items-start py-2 space-x-1  ">
             <CiGlobe className="text-2xl text-deep-orange-400"/>
             <span className="text-deep-orange-700">EN</span>
           <button className="p-1 rounded-full bg-deep-orange-100"><CiSearch className="text-black text-xl"/></button>
+        </div>
         </div>
       </header>
     </>
